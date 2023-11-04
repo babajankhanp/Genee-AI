b2c6e438f058d8880e0595a228d77d030cd791af
babajankhan <babajan.patan@wealthy.in>
2023-11-04 19:28:43 +0530

    added name cards

--- components/landing-content.tsx
+++ components/landing-content.tsx
@@ -4,25 +4,25 @@ import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 
 const testimonials = [
   {
-    name: "Joel",
+    name: "Babajan Khan",
     avatar: "J",
     title: "Software Engineer",
     description: "This is the best application I've ever used!",
   },
   {
-    name: "Antonio",
+    name: "Tony Stark",
     avatar: "A",
     title: "Designer",
     description: "I use this daily for generating new photos!",
   },
   {
-    name: "Mark",
-    avatar: "M",
+    name: "Taylor Swift",
+    avatar: "T",
     title: "CEO",
     description: "This app has changed my life, cannot imagine working without it!",
   },
   {
-    name: "Mary",
+    name: "Mark Zuckerberg",
     avatar: "M",
     title: "CFO",
     description: "The best in class, definitely worth the premium subscription!",
